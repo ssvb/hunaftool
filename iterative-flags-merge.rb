11f5a5c80d72662fb0295978652427d073a08d14
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2025-06-21 16:01:20 +0300

    bugfix for exit

--- iterative-flags-merge.rb
+++ iterative-flags-merge.rb
@@ -4,6 +4,7 @@ while true
   `./flagsfreq words_final.dic words_final.aff > best_multibundle.aff`
   if $? != 0
     STDERR.puts "== Nothing to improve anymore =="
+    exit 0
   end
   `cat words_final.aff best_multibundle.aff > tmp.aff`
   `./hunaftool tmp.aff words.txt words_final.dic`
