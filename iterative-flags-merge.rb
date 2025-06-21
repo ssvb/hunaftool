cae4e6577d8bff455fcc66cc7a98d771e52d95fc
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2025-06-21 15:47:39 +0300

    Iteratively merge flags for the dictionary size reduction

---
+++ iterative-flags-merge.rb
@@ -0,0 +1,15 @@
+round = 0
+while true
+  STDERR.puts "== Round #{round += 1} =="
+  `./flagsfreq words_final.dic words_final.aff > best_multibundle.aff`
+  if $? != 0
+    STDERR.puts "== Nothing to improve anymore =="
+  end
+  `cat words_final.aff best_multibundle.aff > tmp.aff`
+  `./hunaftool tmp.aff words.txt words_final.dic`
+  `./hunaftool tmp.aff words_final.dic words_final.aff`
+  `ls -l words_final.dic`
+  `wc -l words_final.dic`
+  `cat words_final.aff words_final.dic > merged.txt`
+  `ls -l merged.txt`
+end
