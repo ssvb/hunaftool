10f36ea52a177889e8851f57ed10caddd38a3c43
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2025-06-30 07:57:21 +0300

    txt2rul with a new output format

---
+++ txt2rul2.cr
@@ -0,0 +1,169 @@
+#!/usr/bin/env crystal
+# Copyright © 2025 Siarhei Siamashka
+# SPDX-License-Identifier: CC-BY-SA-3.0+ OR MIT
+
+DESIRED_MIN_STRIP_SIZE = 3
+# allow to have a condition field up to this size for zero affixes
+KEEP_COND_SIZE         = 1
+# the maximum number of affixes for a single stem
+MAX_STEM_AFFIXES       = 1000
+# the number of rules
+RULES_LIMIT            = 1000000
+
+module Cfg
+  @@prefix_mode = false
+  def self.prefix_mode?   ; @@prefix_mode end
+  def self.prefix_mode=(v) ; @@prefix_mode = v end
+end
+
+args = ARGV.select do |arg|
+  if arg =~ /^\-p$/
+    Cfg.prefix_mode = true
+    nil
+  elsif arg =~ /^\-/
+    abort "Unrecognized command line option: '#{arg}'\n"
+  else
+    arg
+  end
+end
+
+unless args.size >= 1
+  abort "Need file name in the command line (with the list of words)"
+end
+
+# Yield all possible ways of splitting the word into stem/affix pairs
+def affix_variants(word)
+  0.upto(word.size) do |affsize|
+    yield "#{word[0, word.size - affsize]}/#{word[word.size - affsize, affsize]}"
+  end
+end
+
+def common_prefix_len(word1, word2)
+  ans = 0
+  0.upto({word1.size, word2.size}.min - 1) do |i|
+    return ans if word1[i] != word2[i]
+    ans += 1
+  end
+  return ans
+end
+
+# Run an external GNU coreutils sort process. This can be used to sort really
+# gigantic multi-terabyte sets of data via zstd-compressed temporary files.
+# None of this gigantic data needs to be lifted into RAM, freeing it for the
+# other tasks.
+def pipe_through_coreutils_sort(sortargs = ["--field-separator=/", "--key=1,1", "--key=2", "--compress-program=zstd"])
+  Process.run("sort", args: sortargs, env: {"LC_ALL" => "C"}, input: :pipe, output: :pipe) do |proc|
+    # Adjust pipe configuration knobs to minimize flushing overhead and maximize performance
+    proc.input.sync             = false
+    proc.input.flush_on_newline = false
+    proc.output.read_buffering  = true
+    # Yield the prepared input and output pipes
+    yield proc.input, proc.output
+  end
+end
+
+# This yield all possible stripping/affix combinations for a common stem.
+def affcombs(stem, affixes)
+  if affixes.size > MAX_STEM_AFFIXES
+    if Cfg.prefix_mode?
+      STDERR.puts "! The stem «#{stem.reverse}» has #{affixes.size} prefixes and this exceeds the allowed limit - SKIPPED."
+    else
+      STDERR.puts "! The stem «#{stem}» has #{affixes.size} suffixes and this exceeds the allowed limit - SKIPPED."
+    end
+    return
+  end
+
+    0.upto(affixes.size - 1) do |i|
+      (i + 1).upto(affixes.size - 1) do |j|
+        len = common_prefix_len(affixes[i], affixes[j])
+        if len == 0 || ((affixes[i].size - len) < DESIRED_MIN_STRIP_SIZE && len <= KEEP_COND_SIZE)
+          yield "#{affixes[i].join}/#{affixes[j].join}/1"
+        end
+        if len == 0 || ((affixes[j].size - len) < DESIRED_MIN_STRIP_SIZE && len <= KEEP_COND_SIZE)
+          yield "#{affixes[j].join}/#{affixes[i].join}/1"
+        end
+      end
+    end
+end
+
+def affixpairs(args)
+  pipe_through_coreutils_sort do |sort_input, sort_output|
+    File.open(args[0]).each_line do |l|
+      l = l.strip
+      if Cfg.prefix_mode?
+        l = l.reverse
+      end
+      affix_variants(l) do |sepaffix|
+        sort_input.puts sepaffix
+      end
+    end
+    sort_input.close
+
+    stem = ""
+    affixes = [['a']].clear
+    sort_output.each_line do |l|
+      a = l.strip.split('/')
+      if a[0] != stem
+        affcombs(stem, affixes) {|affcomb| yield affcomb }
+        stem = a[0]
+        affixes.clear
+      end
+      affixes.push(a[1].chars)
+    end
+    affcombs(stem, affixes) {|affcomb| yield affcomb }
+  end
+end
+
+def combine_counters(args)
+  pipe_through_coreutils_sort do |sort_input, sort_output|
+    affixpairs(args) do |affcomb|
+      sort_input.puts affcomb
+    end
+    sort_input.close
+
+    p1 = ""
+    p2 = ""
+    cnt = 0
+    sort_output.each_line do |l|
+      a = l.strip.split('/')
+      if a[0] == p1 && a[1] == p2
+        cnt += a[2].to_i
+      else
+        yield "#{p1}/#{p2}/#{cnt}" if cnt > 0
+        p1 = a[0]
+        p2 = a[1]
+        cnt = a[2].to_i
+      end
+    end
+    yield "#{p1}/#{p2}/#{cnt}" if cnt > 0
+  end
+end
+
+STDOUT.flush_on_newline = false
+STDOUT.sync = false
+pipe_through_coreutils_sort(["--field-separator=/",
+                             "--key=3,3nr", "--key=1,2",
+                             "--compress-program=zstd"]) do |sort_input, sort_output|
+  combine_counters(args) {|line| sort_input.puts line }
+  sort_input.close
+
+  rules_cnt = 0
+  out_data = [[""]].clear
+  out_col_width = [0, 0, 0]
+  sort_output.each_line do |l|
+    a = l.strip.split('/')
+    if Cfg.prefix_mode?
+      a[0] = a[0].reverse
+      a[1] = a[1].reverse
+    end
+    if a[2].to_i > 1
+      out_data.push([a[0] == "" ? "0" : a[0], a[1] == "" ? "0" : a[1], "hunaftool:freq=#{a[2]}"])
+      out_col_width.each_index {|i| out_col_width[i] = out_data.last[i].size if out_data.last[i].size > out_col_width[i] }
+      break if (rules_cnt += 1) >= RULES_LIMIT
+    end
+  end
+  sort_output.close
+  out_data.each do |row|
+    printf("#%s  %#{out_col_width[0]}s %-#{out_col_width[1]}s  %s\n", (Cfg.prefix_mode? ? "prefix" : "suffix"), row[0], row[1], row[2])
+  end
+end
